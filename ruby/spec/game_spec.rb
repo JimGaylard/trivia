06e900648febe867be865852f58ef3b3486e1d0f
jgaylard <jim.gaylard@rea-group.com>
2014-03-29 16:20:20 +1100

    Science

--- ruby/spec/game_spec.rb
+++ ruby/spec/game_spec.rb
@@ -9,6 +9,11 @@ module UglyTrivia
         expect(Game.pure_current_category(4)).to eq("Pop")
         expect(Game.pure_current_category(8)).to eq("Pop")
       end
+      it "should return Pop for 1, 5, 9" do
+        expect(Game.pure_current_category(1)).to eq("Science")
+        expect(Game.pure_current_category(5)).to eq("Science")
+        expect(Game.pure_current_category(9)).to eq("Science")
+      end
     end
   end
 end
