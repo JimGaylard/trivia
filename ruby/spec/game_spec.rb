40cac7ee5b2e03bd76a960e432a989f7bb673fbb
jgaylard <jim.gaylard@rea-group.com>
2014-03-29 16:16:11 +1100

    passing

--- ruby/spec/game_spec.rb
+++ ruby/spec/game_spec.rb
@@ -4,8 +4,10 @@ require_relative '../lib/ugly_trivia/game'
 module UglyTrivia
   describe Game do
     describe "#pure_current_category" do
-      it "should work" do
-      expect(Game.pure_current_category(0)).to eq("Pop")
+      it "should return Pop for 0, 4, 8" do
+        expect(Game.pure_current_category(0)).to eq("Pop")
+        expect(Game.pure_current_category(4)).to eq("Pop")
+        expect(Game.pure_current_category(8)).to eq("Pop")
       end
     end
   end
